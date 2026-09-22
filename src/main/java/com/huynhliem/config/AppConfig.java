05fc4aab48f8f3cb4aab9844b0fa59967b54b33f
huynhliem <liemhuynh0789@gmail.com>
2026-09-22 15:48:02 +0700

    fix: delete config prod

--- src/main/java/com/huynhliem/config/AppConfig.java
+++ src/main/java/com/huynhliem/config/AppConfig.java
@@ -53,7 +53,7 @@ public class AppConfig {
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
-                "/v3/api-docs.yaml", "/v3/api-docs.yml", "/h2-console/**");
+                "/v3/api-docs.yaml", "/v3/api-docs.yml", "/h2-console/**", "/actuator/**");
     }
 
     @Bean
